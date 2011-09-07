8106c1570077f24059fb8c4114e3d93279cc5556
Rudy Sarmiento <rudy750@gmail.com>
2011-09-07 18:04:13 -0400

    added button functionality tutorial 10

--- src/com/play/youtube/NewBostonTutorialsActivity.java
+++ src/com/play/youtube/NewBostonTutorialsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewBostonTutorialsActivity extends Activity {
     /** Called when the activity is first created. */
@@ -21,22 +22,22 @@ public class NewBostonTutorialsActivity extends Activity {
         counter =0;
         add = (Button) findViewById(R.id.bAdd);
         sub = (Button) findViewById(R.id.bSubtract);
-        display = (TextView) findViewById(R.id.tvDisplay);
+        display = (TextView) findViewById(R.id.tvDisplayCount);
         
         add.setOnClickListener(new View.OnClickListener() {
 			
-			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+				counter ++;
+				display.setText("Your total is: " + counter);
+				Toast.makeText(add.getContext(), "add " + counter, 2);
 			}
 		});
         
         sub.setOnClickListener(new View.OnClickListener() {
 			
-			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				counter --;
 				
 			}
 		});
